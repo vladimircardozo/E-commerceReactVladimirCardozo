800674a7e5a4444979652966a00134edc1ac9de3
vladimircardozo <vladimircardozo110@gmail.com>
2024-07-05 19:49:01 -0300

    primer grabacion

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.css';
+
+function NavBar () {
+   return (
+    <nav>
+
+    </nav>
+
+   );
+}
+
+export default NavBar;
