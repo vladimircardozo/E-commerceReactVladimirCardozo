f9db0fc77a41d2a5b162f7e0e19f470b1b5c3588
vladimircardozo <vladimircardozo110@gmail.com>
2024-07-10 13:55:21 -0300

    navbar con la imagen y nombre

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import './css/navbar.css';
 import { CartWidget } from './CartWidget';
+import logo from "../assets/img/logo.jpg";
 
 export const NavBar = () => {
   return (
     <nav className="container navbar navbar-expand-lg mt-3 border border-2 rounded">
       <div className="container-fluid">
         <div className="navbar-header">
-          <p className="navbar-brand text-center m-0">LOGO</p>
+          <img src={logo} alt="logo de la pagina" className='logo' />
         </div>
 
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link hvr-underline-from-center" href="">
               Inicio
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link hvr-underline-from-center" href="">
               Productos
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <a className="nav-link hvr-underline-from-center" href="">
               Nosotros
             </a>
           </li>
